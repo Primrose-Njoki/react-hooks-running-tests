8bb189ce18d33e02ebd572c9d7457793fdfb3c68
Primrose <primrose.njoki@student.moringaschool.com>
2025-04-11 15:29:33 +0300

    final running test

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,10 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  screen.debug(); // This should print the DOM to terminal
+
+  console.log("DEBUG COMPLETE"); // This will help confirm if screen.debug() is running
+
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -7,5 +7,10 @@ import Header from "../components/Header";
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
 
+  screen.debug(); // This should print the DOM to terminal
+
+  console.log("DEBUG COMPLETE"); // This will help confirm if screen.debug() is running
+
+
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
